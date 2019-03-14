c34d397cbe560bf934995d2d575502f6815a3d73
Anil G <anilraj659@gmail.com>
2019-03-14 18:54:07 +0530

    Add files via upload

--- legalAreaList/legalAreaListController.js
+++ legalAreaList/legalAreaListController.js
@@ -21,6 +21,27 @@ spApp.controller("legalAreaListController", ['$scope','$rootScope', function ($r
 
                     $scope.legalAreaList.push(legalAreaObj);
                 });
+                var count = 0;
+                $scope.lpgArea = [];
+                $scope.lpgArea[0] = $scope.legalAreaList.slice(0, 6);
+                $scope.lpgArea[1] = $scope.legalAreaList.slice(6, 12);
+                $scope.lpgArea[2] = $scope.legalAreaList.slice(12);
+
+
+                // $scope.legalAreaList.map(function(item,index){
+                //     count++;
+                //     count+=item.subArea.length;
+                //     if(index+1  === $scope.legalAreaList.length ){
+                //         $scope.lpgArea[count].push(...myArray.slice(lastIndex, index));
+                //     } else if(count = 30 ){
+                //         $scope.lpgArea[count].push(...myArray.slice(lastIndex, index));
+                //         var lastIndex = index;
+                //     } else if ( count = 15){
+                //         $scope.lpgArea[count].push(...myArray.slice(0, index));
+                //         var lastIndex = index;
+                //     }
+                    
+                // });
                 // $scope.$apply();
 
             }
